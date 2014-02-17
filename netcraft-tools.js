2f639cee0cdbe6b0f31254bd644d716f39f3e3f6
krulik <iwillkissyou@gmail.com>
2014-02-17 16:28:31 +0200

    Better type checking

--- netcraft-tools.js
+++ netcraft-tools.js
@@ -7,7 +7,7 @@
 		Execute a function immediately or after a delay
 	*/
 	ncTools.execute = function (func, delay) {
-		if (typeof(delay) !== "undefined") {
+		if (typeof(delay) === "number") {
 			window.setTimeout(func, delay);
 		} else {
 			func();
